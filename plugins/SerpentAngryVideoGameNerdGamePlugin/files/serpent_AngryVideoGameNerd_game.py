56b1516970212027cd18403efa256327b20ef71d
rtre84 <rtre84@users.noreply.github.com>
2019-05-31 12:52:13 +0530

    First Commit.

---
+++ plugins/SerpentAngryVideoGameNerdGamePlugin/files/serpent_AngryVideoGameNerd_game.py
@@ -0,0 +1,53 @@
+from serpent.game import Game
+
+from .api.api import AngryVideoGameNerdAPI
+
+from serpent.utilities import Singleton
+
+
+
+
+class SerpentAngryVideoGameNerdGame(Game, metaclass=Singleton):
+
+    def __init__(self, **kwargs):
+        kwargs["platform"] = "steam"
+
+        kwargs["window_name"] = "AVGN2"
+
+        kwargs["app_id"] = "409660"
+        kwargs["app_args"] = None
+
+
+
+
+        super().__init__(**kwargs)
+
+        self.api_class = AngryVideoGameNerdAPI
+        self.api_instance = None
+
+    @property
+    def screen_regions(self):
+        regions = {
+            "SAMPLE_REGION": (0, 0, 0, 0)
+        }
+
+        return regions
+
+    @property
+    def ocr_presets(self):
+        presets = {
+            "SAMPLE_PRESET": {
+                "extract": {
+                    "gradient_size": 1,
+                    "closing_size": 1
+                },
+                "perform": {
+                    "scale": 10,
+                    "order": 1,
+                    "horizontal_closing": 1,
+                    "vertical_closing": 1
+                }
+            }
+        }
+
+        return presets
